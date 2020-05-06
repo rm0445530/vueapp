85a50c91a327629d3c6ae3726e241192013063a1
rm0445530 <58572356+rm0445530@users.noreply.github.com>
2020-05-06 18:25:27 -0500

    Added TaskList Component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <h1>{{theTitle}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props:['theTitle']
+}
+</script>
+
+<style scoped>
+    .header{
+        background: #333;
+        color: white;
+        text-align: center;
+        padding: 10px;
+        margin: 0px;
+    }
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>{{theTitle}}</h1>
+        <p v-for="task in taskList" v-bind:key="task.id">{{task.titel}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TaskList",
+    props:['theTitle', 'taskList']
+}
+</script>
