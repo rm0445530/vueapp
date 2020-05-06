da7530e3051f651f1e15d771f6c5536120e56b81
rm0445530 <58572356+rm0445530@users.noreply.github.com>
2020-05-06 18:20:29 -0500

    created TaskList component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
   <div id="app">
     <Header theTitle="Vue to Due"/>
+    <TaskList theTitle="My Task" v-bind:taskList='task'/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import TaskList from "./components/TaskList.vue";
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    TaskList
+  },
+  data(){
+    return{
+      task:[
+        {id:1, titel:"Learn HTML & CSS", completed:false},
+        {id:2, titel:"Learn Javascript", completed:true},
+        {id:3, titel:"Learn a JS Framework", completed:false}
+      ]
+    }
   }
 }
 </script>
